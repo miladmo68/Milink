baff5f351151fa354e588a1595aa3342ebf42b78
miladmo68 <miladmo68@gmail.com>
2025-08-01 11:54:49 -0400

    Logo changed

--- src/main.jsx
+++ src/main.jsx
@@ -3,16 +3,6 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
-// ✅ Force light theme BEFORE React renders
-if (!localStorage.getItem("theme")) {
-  document.documentElement.setAttribute("data-theme", "emerald"); // Always start light
-} else {
-  document.documentElement.setAttribute(
-    "data-theme",
-    localStorage.getItem("theme")
-  );
-}
-
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
