947671852f8fc691f92c5974d38c0acd44900879
miladmo68 <miladmo68@gmail.com>
2025-09-16 19:21:24 -0400

    changed

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ export default function App() {
     <>
       {/* <Navbar /> */}
       {/* <Hero /> */}
-      <About />
+      {/* <About /> */}
       <Services />
       <Portfolio />
       <Testimonials />
