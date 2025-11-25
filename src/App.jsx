0ac9bd076a9cddf4891597671a35ecc1c0ca5eec
miladmo68 <miladmo68@gmail.com>
2025-11-25 09:55:45 -0500

    Updated

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ export default function App() {
       {/* <Portfolio /> */}
       {/* <Testimonials /> */}
       {/* <CTA /> */}
-      {/* <Contact /> */}
+      <Contact />
       <Footer />
     </>
   );
