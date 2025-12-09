6d4a6dcd501f8f68ab71c190f5fe48b6f0edf6a8
miladmo68 <miladmo68@gmail.com>
2025-12-09 10:28:22 -0500

    Updated

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ import Footer from "./components/Footer";
 export default function App() {
   return (
     <>
-      {/* <Navbar /> */}
+      <Navbar />
       <Hero />
       {/* <About /> */}
       <Services />
